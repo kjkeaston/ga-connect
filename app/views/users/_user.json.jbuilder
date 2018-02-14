df994d81295ad710a665ee8e45178f52a89b9184
Kyle Easton <kyleeaston@Kyles-MacBook-Air.local>
2018-02-14 12:07:11 -0800

    seed file with data

--- app/views/users/_user.json.jbuilder
+++ app/views/users/_user.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! user, :id, :name, :role, :skills, :portfolio, :linkedin, :email, :photo, :created_at, :updated_at
+json.extract! user, :id, :first_name, :last_name, :role, :skills, :portfolio, :linkedin, :email, :photo, :created_at, :updated_at
 json.url user_url(user, format: :json)
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,80 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 User.destroy_all
-User.create(name: "John", role: "Engineer", skills: "HTML, CSS, Javascript, React, Express", portfolio: "https://www.google.com", linkedin: "https://www.linkedin.com", email: "john@email.com", photo: "https://www.sparklabs.com/forum/styles/comboot/theme/images/default_avatar.jpg")
+# User.create(name: "John", role: "Engineer", skills: "HTML, CSS, Javascript, React, Express", portfolio: "https://www.google.com", linkedin: "https://www.linkedin.com", email: "john@email.com", photo: "https://www.sparklabs.com/forum/styles/comboot/theme/images/default_avatar.jpg")
 
 
+def random_role
+  ["WDI", "UXDI", "DSI", "iOS"].sample
+end
 
+# Users
+users_data = []
+10.times do
+  first = FFaker::Name.first_name
+  last = FFaker::Name.last_name
+  users_data << {
+    first_name: first,
+    last_name: last,
+    role: random_role,
+    skills: "HTML, CSS, Javascript, Bootstrap, Zeplin, Jira, Invision, Adobe Suite, Node.js, Express, Sketch, React, Python",
+    portfolio: "https://github.com/",
+    linkedin: "https://www.linkedin.com",
+    email: "#{first[0]}_#{last}@example.com".downcase,
+    photo: "https://www.linkedin.com"
+  }
+end
 
-# # Users
-# users_data = []
-# 4.times do
-#   first = FFaker::Name.first_name
-#   last = FFaker::Name.last_name
-#   users_data << {
-#     name: first
-#     last_name: last,
-#     current_city: "San Francisco",
-#     email: "#{first[0]}_#{last}@example.com".downcase,
-#     password: "apple"
-#   }
-# end
-# users = User.create(users_data)
-
-# def random_city
-#   ["San Francisco", "London", "Gibraltar"].sample
-# end
-
-# # Cities
-# cities_data = []
-#   cities_data << {
-#     id: 2,
-#     name: "San Francisco",
-#     city_image: "san_francisco.jpg"
-#   }
-#   cities_data << {
-#     id: 1,
-#     name: "London",
-#     city_image: "london.jpg"
-#   }
-#   cities_data << {
-#     id: 3,
-#     name: "Gibraltar",
-#     city_image: "gibraltar2.jpg"
-#   }
-# cities = City.create(cities_data)
-
-# # "Below for randomizing city names"
-# # cities_data = []
-# # 3.times do
-# #   cities_data << {
-# #     name: random_city,
-# #     city_image: "image tag"
-# #   }
-# # end
-# # cities = City.create(cities_data)
-
-# # Posts
-# posts_data = []
-# 6.times do
-#   posts_data << {
-#     user: users.sample,
-#     city: cities.sample,
-#     title: "My family had a great time!",
-#     content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-#             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-#             quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-#             consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-#             cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-#             proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
-#   }
-# end
-# posts = Post.create(posts_data)
+users = User.create(users_data)
